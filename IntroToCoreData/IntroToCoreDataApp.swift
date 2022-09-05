df95edfcfcefef99dbba67440bb1640d1ac8cf42
AndreaMonroy <andrearamonroy@gmail.com>
2022-09-05 12:49:59 -0400

    Initial Commit

---
+++ IntroToCoreData/IntroToCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToCoreDataApp.swift
+//  IntroToCoreData
+//
+//  Created by Andrea on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct IntroToCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
